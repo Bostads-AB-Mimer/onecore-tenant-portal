4553c36b4b909b144c1cd63133ad5b5af0f6b66d
Johanna Grahn <johanna.grahn@iteam.se>
2023-05-17 14:16:09 +0200

    MUI Material UI

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,15 +1,43 @@
+import {
+  ChatOutlined,
+  ChecklistOutlined,
+  HomeOutlined,
+  MenuOutlined,
+} from '@mui/icons-material';
+import {
+  AppBar,
+  BottomNavigation,
+  BottomNavigationAction,
+  IconButton,
+  Toolbar,
+} from '@mui/material';
+
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <header className="w-full max-w-md flex flex-col items-center">
-        <h1 className="text-2xl font-bold">
-          Hello Iteam + React + Typescript!
-        </h1>
-        <img
-          src="https://cdn-assets-cloud.frontify.com/local/frontify/eyJwYXRoIjoiXC9wdWJsaWNcL3VwbG9hZFwvc2NyZWVuc1wvMTUzOTg2XC9lZWNmNjExMDg1YTI1YWM0MTIzZGE3NmY4M2EzZTdkNi0xNTEyNzI5Mzk3LnBuZyJ9:frontify:ANleqg1_q50wrm-EmaePRoqAtTkpInN90J70KGuqUbU?width=449"
-          alt="logo"
-        />
-      </header>
+    // items-center justify-center
+    <div className="flex h-screen w-screen flex-col ">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start">
+            <MenuOutlined></MenuOutlined>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <BottomNavigation>
+        <BottomNavigationAction
+          label="Alt 1"
+          icon={<HomeOutlined></HomeOutlined>}
+        ></BottomNavigationAction>
+        <BottomNavigationAction
+          label="Alt 2"
+          icon={<ChatOutlined></ChatOutlined>}
+        ></BottomNavigationAction>
+        <BottomNavigationAction
+          label="Alt 3"
+          icon={<ChecklistOutlined></ChecklistOutlined>}
+        ></BottomNavigationAction>
+      </BottomNavigation>
     </div>
   );
 }
