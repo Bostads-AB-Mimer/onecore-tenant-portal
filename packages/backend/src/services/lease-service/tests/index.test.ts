168831693425cd9e1b967b67f1c925e30d7370aa
Anders Norrback Bornholm <anders@bornholm.se>
2023-05-26 09:21:14 +0200

    Renamed route for lease

---
+++ packages/backend/src/services/lease-service/tests/index.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import Koa from 'koa'
+import KoaRouter from '@koa/router'
+import bodyParser from 'koa-bodyparser'
+import { routes } from '../index'
+import * as randomDataApiAdapter from '../adapters/tenant-lease-adapter'
+
+const app = new Koa()
+const router = new KoaRouter()
+routes(router)
+app.use(bodyParser())
+app.use(router.routes())
+
+describe('lease-service', () => {
+  describe('GET /my-lease', () => {
+    it('responds', async () => {
+      const res = await request(app.callback()).get('/my-lease')
+      expect(res.status).toBe(200)
+      expect(res.body.data.lease).toBeDefined()
+    })
+  })
+})
