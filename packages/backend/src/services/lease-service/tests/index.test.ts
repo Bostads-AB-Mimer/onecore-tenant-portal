ec64b82b3608436b9bacf55b94a56687d2e05202
Johanna Grahn <johanna.grahn@iteam.se>
2023-12-22 15:26:38 +0100

    Added test

--- packages/backend/src/services/lease-service/tests/index.test.ts
+++ packages/backend/src/services/lease-service/tests/index.test.ts
@@ -4,18 +4,45 @@ import KoaRouter from '@koa/router'
 import bodyParser from 'koa-bodyparser'
 import { routes } from '../index'
 
+import * as coreAdapter from '../adapters/core-adapter'
+
 const app = new Koa()
 const router = new KoaRouter()
 routes(router)
 app.use(bodyParser())
 app.use(router.routes())
 
-describe('lease-service', () => {
-  describe('GET /my-lease', () => {
+describe('lease-service index', () => {
+  describe('GET /material-choices', () => {
     it('responds', async () => {
-      const res = await request(app.callback()).get('/my-lease')
+      const materialChoicesSpy = jest
+        .spyOn(coreAdapter, 'getMaterialChoices')
+        .mockResolvedValue({
+          roomTypes: [
+            {
+              roomTypeId: 'BADRUM',
+              name: 'BADRUM',
+              materialOptionGroups: [Array],
+            },
+            { roomTypeId: 'KÖK', name: 'KÖK', materialOptionGroups: [Array] },
+            {
+              roomTypeId: 'RUM 1',
+              name: 'RUM 1',
+              materialOptionGroups: [Array],
+            },
+            {
+              roomTypeId: 'VARDAGSRUM',
+              name: 'VARDAGSRUM',
+              materialOptionGroups: [Array],
+            },
+          ],
+        })
+
+      const res = await request(app.callback()).get('/material-choices')
+
       expect(res.status).toBe(200)
-      expect(res.body.data.lease).toBeDefined()
+      expect(materialChoicesSpy).toHaveBeenCalled()
+      expect(res.body.data.roomTypes).toBeDefined()
     })
   })
 })
