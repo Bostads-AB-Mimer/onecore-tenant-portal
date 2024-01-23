648ba2744c11a1380f48680affcf3d7590aa1d17
Johanna Grahn <johanna.grahn@iteam.se>
2024-01-23 14:49:39 +0100

    Fix 1095 package updates
    * Re-added tests and made them pass

--- packages/backend/src/middlewares/error-handler.test.ts
+++ packages/backend/src/middlewares/error-handler.test.ts
@@ -9,20 +9,19 @@ describe('errorHandler', () => {
     app.use(errorHandler())
   })
 
-  test.todo('Fix this test')
-  // it('catches errors and passes the error message along to the response', async () => {
-  //   app.use(async (ctx, next) => {
-  //     throw new Error('I should be caught')
-  //   })
-  //   app.use(async (ctx, next) => {
-  //     ctx.status = 200
-  //     ctx.body = 'All is good'
-  //   })
-  //   const response = await request(app.callback()).get('/')
-  //   expect(response.status).toBe(500)
-  //   expect(response.body).toStrictEqual({
-  //     errorMessage: 'I should be caught',
-  //     message: 'Internal server error',
-  //   })
-  // })
+  it('catches errors and passes the error message along to the response', async () => {
+    app.use(async (ctx, next) => {
+      throw new Error('I should be caught')
+    })
+    app.use(async (ctx, next) => {
+      ctx.status = 200
+      ctx.body = 'All is good'
+    })
+    const response = await request(app.callback()).get('/')
+    expect(response.status).toBe(500)
+    expect(response.body).toStrictEqual({
+      errorMessage: 'I should be caught',
+      message: 'Internal server error',
+    })
+  })
 })
